6666eaf45399ea2bb8563c77a1d7741af8fe7358
xxljunjun <liaojunjun@autel.com>
2021-06-28 11:38:27 +0800

    0

--- src/pages/studyvue/donghua.vue
+++ src/pages/studyvue/donghua.vue
@@ -2,14 +2,32 @@
   <div class="donghua">
     <button @click="dianji" class="btn">点我呀</button>
     <div :class="status ? 'box clickbox' : 'box'"></div>
+    <hr />
+    <button v-on:click="show = !show">Toggle</button>
+    <transition name="fade">
+      <p v-if="show">hello</p>
+    </transition>
   </div>
 </template>
 
 <script>
+/* 
+  ==>vue封装了transition标签
+
+    ==>v-enter
+    ==>v-enter-active
+    ==>v-enter-to
+
+    ==>v-leave
+    ==>v-leave-active
+    ==>v-leave-to
+
+*/
 export default {
   data() {
     return {
       status: false,
+      show: true,
     }
   },
   methods: {
@@ -21,6 +39,13 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
 .donghua {
   .box {
     height: 200px;
