a67d35b6162193502c84887654d32e16e7ee380b
xxljunjun <liaojunjun@autel.com>
2021-06-29 10:58:24 +0800

    实现vue的倒计时功能

--- src/pages/ceshi/index.vue
+++ src/pages/ceshi/index.vue
@@ -1,19 +1,49 @@
 <template>
   <div class="other">
-      222222222222222
+    <div>倒计时功能实现</div>
+    <div>{{ `${day}天 ${hr}小时 ${min}分钟 ${sec}秒` }}</div>
   </div>
 </template>s
 
 <script>
-
 export default {
   data() {
     return {
-
+      day: 0,
+      hr: 0,
+      min: 0,
+      sec: 0,
+      timer: null,
     }
   },
   components: {},
-  methods: {},
+  mounted() {
+    this.countdown()
+  },
+  destroyed() {
+    clearTimeout(this.timer)
+  },
+  methods: {
+    countdown() {
+      const end = Date.parse(new Date('2021-06-30'))
+      const now = Date.parse(new Date())
+      //格林威治时间
+      console.log('获取当前的格林威治时间', now)
+      const msec = end - now
+      let day = parseInt(msec / 1000 / 60 / 60 / 24)
+      let hr = parseInt((msec / 1000 / 60 / 60) % 24)
+      let min = parseInt((msec / 1000 / 60) % 60)
+      let sec = parseInt((msec / 1000) % 60)
+      this.day = day
+      this.hr = hr > 9 ? hr : '0' + hr
+      this.min = min > 9 ? min : '0' + min
+      this.sec = sec > 9 ? sec : '0' + sec
+      const that = this
+      this.timer = setTimeout(function () {
+        that.countdown()
+      }, 1000)
+    },
+  },
 }
 </script>
 
