2c5f4cbbec862b969fa09d6902ad2d9297747f0a
xxljunjun <793117268@qq.com>
2021-11-26 17:46:37 +0800

    allvue文件优化

--- src/pages/studyvue/all_name.vue
+++ src/pages/studyvue/all_name.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="allVue">
-    {{val}}
-    {{one_name}}
-    <Child 
-      :val='val' 
-      :one_name='one_name' 
-      :two_name='two_name'
-      @changeVal='changeVal'
-      @changeName='changeName'
+    <div v-html="newVal"></div>
+    {{ val }}
+    {{ one_name }}
+    <Child
+      :val="val"
+      :one_name="one_name"
+      :two_name="two_name"
+      @changeVal="changeVal"
+      @changeName="changeName"
     />
   </div>
 </template>
@@ -18,55 +19,89 @@
 //$listeners里存放的是父组件中绑定的非原生事件。
 //inheritAttrs选项则为是否挂载到组件元素的attribute。
 //子组件的顶层标签元素中（本例子的div元素）会渲染出父组件传递过来的属性
-import Child from './components/child.vue'
+import Child from "../reviewVue/components/child.vue";
+import { mymixin } from "@/components/mixin"; //局部混入方法和data
 export default {
-  provide() {
-    return {
-      val: '我是所有allVue组件中的数据',
-    }
-  },
-  props: {
-    //父子组件传值
-  },
-  data() {
-    return {
-      val:'我是父组件的val',
-      one_name:'xxl',
-      two_name:'junjun',
-      //声明变量
-      //vm.$data 访问原始数据对象。Vue 实例也代理了 data 对象上所有的 property，因此访问 vm.a 等价于访问 vm.$data.a。
-      //节制地使用 $parent 和 $children - 它们的主要目的是作为访问组件的应急方法。更推荐用 props 和 events 实现父子组件通信
-    }
-  },
-  watch: {
-    //监控路由
-  },
-  computed: {
-    //vue的计算属性的 getter
-  },
-  directives: {
-    //局部自定义指令
-  },
-  mixins: [
-    //局部混入
-  ],
-  filters: {
-    //过滤器
-  },
-  methods: {
-    //函数function
-    changeVal(val){
-      console.log('val',val)
-      this.val = val
-    },
-    changeName(name){
-      console.log('name',name)
-      this.one_name = name
-    }
-  },
   components: {
     Child,
     //局部注册组件
+  },
+  provide() {
+    return {
+      val: "我是所有allVue组件中的数据",
+    };
+  },
+  props: {
+    //父子组件传值
+    my_name: {
+      type: String,
+      default: "",
+    },
+  },
+  data() {
+    return {
+      val: "我是父组件的val",
+      one_name: "xxl",
+      two_name: "junjun",
+      //声明变量
+      //vm.$data 访问原始数据对象。Vue 实例也代理了 data 对象上所有的 property，因此访问 vm.a 等价于访问 vm.$data.a。
+      //节制地使用 $parent 和 $children - 它们的主要目的是作为访问组件的应急方法。更推荐用 props 和 events 实现父子组件通信
+    };
+  },
+  watch: {
+    //监控路由
+    val: {
+      handler(newVal) {
+        console.log(newVal);
+      },
+      deep: true,
+      immediate: true,
+    },
+  },
+  computed: {
+    //vue的计算属性的 getter
+    newVal() {
+      return this.val.split("").reverse().join("");
+    },
+  },
+  directives: {
+    //局部自定义指令
+    focus: {
+      // 指令的定义
+      inserted: function (el, binding) {
+        el.focus();
+        console.log(el);
+        console.log(binding);
+      },
+      // bind: function () {},
+      // inserted: function () {},
+      // update: function () {},
+      // componentUpdated: function () {},
+      // unbind: function () {},
+    },
+  },
+  mixins: [
+    //局部混入
+    mymixin,
+  ],
+  filters: {
+    //过滤器
+    numFilters: function (val, a, b) {
+      console.log(val);
+      console.log(a);
+      console.log(b);
+    },
+  },
+  methods: {
+    //函数function
+    changeVal(val) {
+      console.log("val", val);
+      this.val = val;
+    },
+    changeName(name) {
+      console.log("name", name);
+      this.one_name = name;
+    },
   },
   // render: (h) => {
   //   //用js语言来构建DOM  createElement  h  _c  ---------都是别称
@@ -86,7 +121,7 @@ export default {
   created() {},
   beforeMount() {},
   mounted() {
-    console.log("111111111",this)
+    // console.log("111111111",this)
   },
   beforeUpdate() {},
   updated() {},
@@ -118,7 +153,7 @@ export default {
   //   // 导航离开该组件的对应路由时调用
   //   // 可以访问组件实例 `this`
   // },
-}
+};
 </script>
 
 <style lang="scss" scoped>
