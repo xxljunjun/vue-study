93b41f4f7399644921fb918e20e09d5f6611dac5
xxljunjun <793117268@qq.com>
2023-04-13 10:01:40 +0800

    安装了lodash库

---
+++ src/pages/lodashtext/index.vue
@@ -0,0 +1,45 @@
+<template>
+    <div >
+     <button @click="clickMe">点我打印</button>
+    </div>
+  </template>
+  
+  <script>
+  import {isArray,get} from 'lodash'
+
+  
+  export default {
+    data() {
+      return {
+       
+      };
+    },
+    components: {
+    },
+    beforeMount() {
+      
+    },
+    methods:{
+        clickMe(){
+            // let arr = []
+            // console.log(isArray(arr))
+            var object = { 'a': [{ 'b': { 'c': 3 } }] };
+ 
+// _.get(object, 'a[0].b.c');
+// // => 3
+ 
+// _.get(object, ['a', '0', 'b', 'c']);
+// // => 3
+ 
+// _.get(object, 'a.b.c', 'default');
+console.log(get(object,'a[0].b.c'))
+
+        }
+    }
+  };
+  </script>
+  
+  <style lang="scss" scoped>
+
+  </style>
+  
